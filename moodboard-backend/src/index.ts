415bee6d95a585d201879356dfb7cd5f2c97c4ed
Amir Ghari <amir.ghari@metropolia.fi>
2025-03-29 21:58:12 +0200

    backend structure is updated

--- moodboard-backend/src/index.ts
+++ moodboard-backend/src/index.ts
@@ -1,14 +1,18 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth';
+import journalRoutes from './routes/journal';
 
 dotenv.config();
 
 const app = express();
-const PORT: number | string = process.env.PORT || 5000;
+const PORT: number | string = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/journal', journalRoutes);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('MoodBoard API is running');
