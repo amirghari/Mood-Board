3f0b81e2e9adefe956a9a06b61ea8654121b1a92
Amir Ghari <amir.ghari@metropolia.fi>
2025-04-01 15:15:05 +0300

    Account screen is refactored

--- moonboard/app/screens/AccountScreen.tsx
+++ moonboard/app/screens/AccountScreen.tsx
@@ -1,3 +1,4 @@
+// app/screens/AccountScreen.tsx
 import React from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import Icon from '../components/Icon';
@@ -8,72 +9,70 @@ import ListItem from '../components/ListItem';
 import colors from '../config/colors';
 
 const menuItems = [
-    { 
-        title: "My Journals",
-        icon: {
-            name: "format-list-bulleted" as keyof typeof MaterialCommunityIcons.glyphMap,
-            backgroundColor: colors.primary
-        }
-    },
-    { 
-        title: "My Listings",
-        icon: {
-            name: "format-list-bulleted" as keyof typeof MaterialCommunityIcons.glyphMap,
-            backgroundColor: colors.secondary
-        }
+  { 
+    title: "My Journals",
+    icon: {
+      name: "format-list-bulleted" as keyof typeof MaterialCommunityIcons.glyphMap,
+      backgroundColor: colors.primary
     }
+  }
 ];
 
 interface Props {
-    onEdit: () => void;
+  onEdit: () => void;
+  onLogout: () => void;
+  user: {
+    name: string;
+    username: string;
+  }
 }
 
-function AccountScreen({ onEdit }: Props) {
-    return (
-        <Screen>
-            <View style={styles.container}>
-                <ListItem 
-                    title='Amir'
-                    subTitle='ghari@gmail.com'
-                    image={require('../assets/user.png')}
+function AccountScreen({ onEdit, onLogout, user }: Props) {
+  return (
+    <Screen>
+      <View style={styles.container}>
+        <ListItem 
+          title={user.name}
+          subTitle={user.username}
+          image={require('../assets/user.png')}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={item => item.title}
+          ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
+          renderItem={({ item }) => (
+            <ListItem 
+              title={item.title}
+              subTitle=""
+              IconComponent={() => (
+                <Icon 
+                  name={item.icon.name} 
+                  backgroundColor={item.icon.backgroundColor}
                 />
-            </View>
-            <View style={styles.container}>
-                <FlatList
-                    data={menuItems}
-                    keyExtractor={item => item.title}
-                    ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
-                    renderItem={({item}) => (
-                        <ListItem 
-                            title={item.title}
-                            subTitle=""
-                            IconComponent={() => (
-                                <Icon 
-                                    name={item.icon.name} 
-                                    backgroundColor={item.icon.backgroundColor}
-                                />
-                            )}
-                            onPress={onEdit}
-                        />
-                    )}
-                />
-            </View>
-            <ListItem
-                title='Log Out'
-                subTitle=""
-                IconComponent={() => (
-                    <Icon name='logout' backgroundColor="red" />
-                )}
+              )}
+              onPress={onEdit}
             />
-        </Screen>
-    );
+          )}
+        />
+      </View>
+      <ListItem
+        title='Log Out'
+        subTitle=""
+        IconComponent={() => (
+          <Icon name='logout' backgroundColor={colors.secondary} />
+        )}
+        onPress={onLogout}
+      />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    container : {
-        marginVertical: 20,
-    },
-
-})
+  container : {
+    marginVertical: 20,
+  },
+});
 
 export default AccountScreen;
